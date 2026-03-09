b3c4c8edcff05f6f275d0ecf17c03e26c6351461
onimusha-dev <musaddikulislam007@gmail.com>
2026-03-09 16:54:56 +0530

    feat: post module with proper zod validation

---
+++ api/src/modules/posts/index.ts
@@ -0,0 +1,4 @@
+export { PostController } from './posts.controller';
+export { createPostRoutes } from './posts.routes';
+export { PostService } from './posts.service';
+export { PostRepository } from './posts.repository';
