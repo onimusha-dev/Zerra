462de07e72e89dffb7662b59658c0045659a6e70
onimusha-dev <musaddikulislam007@gmail.com>
2026-03-17 17:17:19 +0530

    =======refactor whole shit=======

--- web/frontend/next.config.ts
+++ web/frontend/next.config.ts
@@ -1,11 +1,17 @@
-import type { NextConfig } from 'next';
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-const nextConfig: NextConfig = {
+const nextConfig = {
     async rewrites() {
         return [
             {
                 source: '/api/:path*',
-                destination: 'http://localhost:9000/:path*',
+                destination: API_URL ? `${API_URL}/:path*` : 'http://localhost:9000/:path*',
+            },
+            {
+                source: '/uploads/:path*',
+                destination: API_URL
+                    ? `${API_URL}/uploads/:path*`
+                    : 'http://localhost:9000/uploads/:path*',
             },
         ];
     },
